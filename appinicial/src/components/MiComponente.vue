0264b51ccd29221409b875c9dbbac5fc31cf0d0f
Heiner Cufiño <heicufi@gmail.com>
2021-10-06 15:08:12 -0500

    Se agregra un parrafo un nuevo componente

--- appinicial/src/components/MiComponente.vue
+++ appinicial/src/components/MiComponente.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>Mi Componente</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore alias, veritatis mollitia et blanditiis eius aperiam debitis reiciendis doloribus nemo reprehenderit ipsa. Necessitatibus quis sed eligendi, minima eum eveniet numquam.
+  Id amet aut ex dolorum officiis architecto eveniet dolorem excepturi impedit expedita doloribus fugiat iusto, et minima explicabo ea molestias! Architecto vel quae distinctio explicabo esse beatae, sint laboriosam veniam?
+  Excepturi pariatur minus corrupti aliquam soluta voluptatibus quas voluptate quos aliquid optio vero rem deleniti, corporis mollitia dolor! Excepturi corporis error deleniti, aliquid quam similique repellendus perferendis cupiditate voluptates et.</p>
 </template>
 
 <script>
