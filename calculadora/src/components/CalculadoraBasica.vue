f299a7f388637db17197a927b74e6f75a5dec99b
Heiner Cufiño <heicufi@gmail.com>
2021-10-08 08:08:26 -0500

    Proyecto Calculadora con Vue js

---
+++ calculadora/src/components/CalculadoraBasica.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h1>Calculadora Basica</h1>
+    <input type="number">
+    <input type="number">
+    <button>Sumar</button>
+</div>
+</template>
+
+<script>
+export default {
+    name: "CalculadoraBasica",
+    methods: {},
+}
+</script>
+
+<style>
+
+</style>
