379d5b8c82f903833487bc868995a2a8cccf93db
Heiner Cufiño <heicufi@gmail.com>
2021-10-08 08:25:47 -0500

    Proyecto Calculadora con Vue js

--- calculadora/src/components/CalculadoraBasica.vue
+++ calculadora/src/components/CalculadoraBasica.vue
@@ -4,6 +4,7 @@
     <input type="number" v-model="n1">
     <input type="number" v-model="n2">
     <button @click="suma(n1,n2)">Sumar</button>
+    <button @click="resta(n1,n2)">Restar</button>
     <span>{{ resultado }}</span>
 </div>
 </template>
@@ -21,6 +22,9 @@ export default {
     methods: {
         suma(n1,n2){
             this.resultado = parseFloat(this.n1) + parseFloat(this.n2);
+        },
+        resta(n1,n2){
+            this.resultado = parseFloat(this.n1) - parseFloat(this.n2);
         }
     },
 }
