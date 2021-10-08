c6b1650363097135f098e716797138fe0ae0af27
Heiner Cufiño <heicufi@gmail.com>
2021-10-08 11:19:21 -0500

    Proyecto Calculadora con Vue js

--- calculadora/src/components/CalculadoraBasica.vue
+++ calculadora/src/components/CalculadoraBasica.vue
@@ -1,34 +1,66 @@
 <template>
-<div>
-    <h1>Calculadora Basica</h1>
-    <v-text-field
-        type="number"
-        label="Numero 1"
-        :rules="rules"
-        hide-details="auto"
-        v-model="n1"
-    ></v-text-field>
-    <v-text-field
-        type="number"
-        label="Numero 2"
-        :rules="rules"
-        hide-details="auto"
-        v-model="n2"
-    ></v-text-field>
-    <v-btn
-        color="primary"
-        elevation="13"
-        rounded
-        @click="suma(n1,n2)"
-    >Sumar</v-btn>
-        <v-btn
-        color="primary"
-        elevation="13"
-        rounded
-        @click="resta(n1,n2)"
-    >Restar</v-btn>
+<v-container>
+    <v-row>
+        <v-col>
+            <h1 class="text-center">Calculadora Basica</h1>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-col>
+            <v-text-field
+                type="number"
+                label="Numero 1"
+                :rules="rules"
+                hide-details="auto"
+                v-model="n1"
+            ></v-text-field>
+        </v-col>
+        <v-col>
+            <v-text-field
+                type="number"
+                label="Numero 2"
+                :rules="rules"
+                hide-details="auto"
+                v-model="n2"
+            ></v-text-field>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-col>
+            <v-btn
+                color="primary"
+                elevation="13"
+                rounded
+                @click="suma(n1,n2)"
+            >Sumar</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn
+            color="primary"
+            elevation="13"
+            rounded
+            @click="resta(n1,n2)"
+            >Restar</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn
+            color="primary"
+            elevation="13"
+            rounded
+            @click="multiplicacion(n1,n2)"
+            >Multiplicar</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn
+            color="primary"
+            elevation="13"
+            rounded
+            @click="division(n1,n2)"
+            >Dividir</v-btn>
+        </v-col>
+    </v-row>
     <span>{{ resultado }}</span>
-</div>
+</v-container>
 </template>
 
 <script>
@@ -51,6 +83,12 @@ export default {
         },
         resta(n1,n2){
             this.resultado = parseFloat(this.n1) - parseFloat(this.n2);
+        },
+        multiplicacion(n1,n2){
+            this.resultado = parseFloat(this.n1) * parseFloat(this.n2);
+        },
+        division(n1,n2){
+            this.resultado = parseFloat(this.n1) / parseFloat(this.n2);
         }
     },
 }
