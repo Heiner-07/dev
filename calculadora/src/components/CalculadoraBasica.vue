31ce3df8be4b61011b1c955f4752c5ffddf05060
Heiner Cufiño <heicufi@gmail.com>
2021-10-08 08:22:33 -0500

    Proyecto Calculadora con Vue js

--- calculadora/src/components/CalculadoraBasica.vue
+++ calculadora/src/components/CalculadoraBasica.vue
@@ -1,16 +1,28 @@
 <template>
 <div>
     <h1>Calculadora Basica</h1>
-    <input type="number">
-    <input type="number">
-    <button>Sumar</button>
+    <input type="number" v-model="n1">
+    <input type="number" v-model="n2">
+    <button @click="suma(n1,n2)">Sumar</button>
+    <span>{{ resultado }}</span>
 </div>
 </template>
 
 <script>
 export default {
     name: "CalculadoraBasica",
-    methods: {},
+    data:()=>{
+        return{
+            n1: null, 
+            n2: null, 
+            resultado: ""
+        }
+    },
+    methods: {
+        suma(n1,n2){
+            this.resultado = parseFloat(this.n1) + parseFloat(this.n2);
+        }
+    },
 }
 </script>
 
